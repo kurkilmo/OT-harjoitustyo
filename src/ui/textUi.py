687a73f958188e09eede66082f9d734cddac49c7
kurkilmo <ilmo.kurki00gmail.com>
2021-11-22 16:50:18 +0200

    Add classes for reading metadata using exiftool and subprocess

--- src/ui/textUi.py
+++ src/ui/textUi.py
@@ -1,21 +1,25 @@
+import subprocess
 from data.metadataTools import Metadata
+from data.metadata_subprocess import Metadata_subprocess
 
 
 def run():
     cont = True
 
     def read(inp):
-        dataTool = Metadata()
+        dataTool = Metadata_subprocess()
         file = str(inp[1])
         mtd = dataTool.getMetadata(file)
         for item in mtd.keys():
             print(f"{item} --- {mtd[item]}")
 
 
-
+    print("Commands:\nread (file) - read metadata from file. Leave file field empty for example file\nexit - exit program")
     
     while(cont):
         inp = input("Command: ")
+        if str(inp) == 'read':
+            inp = 'read testimage.jpg'
         #inp = "read testailua/ormus.jpg"
         inp = str(inp).split(" ")
 
